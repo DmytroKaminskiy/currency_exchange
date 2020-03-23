c7af21e5e02f83fe2ffc582ad07a55f65029581b
Dmytro Kaminskiy <dmytro.kaminskyi92@gmail.com>
2020-03-23 21:31:52 +0200

    Update start_celerybeat.sh

--- commands/start_celerybeat.sh
+++ commands/start_celerybeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm /srv/project/run/celerybeat.pid
-rm srv/project/run/celerybeat-schedule
+rm /srv/project/run/celerybeat-schedule
 celery -A currency_exchange beat -l info --workdir=/srv/project/src --pidfile=/srv/project/run/celerybeat.pid  --schedule=/srv/project/run/celerybeat-schedule
